b6562e8213e278dc5258f40baee554eb805be3f6
RainbowKiwiFOX <RainbowKiwiFOX@gmail.com>
2015-08-11 17:20:56 +0300

    Запилино удаление сообщений о входе, выходе, перекидывание сообщение о смерти в нотифер

--- src/main/java/com/rainbowkiwifox/deathlogger/Main.java
+++ src/main/java/com/rainbowkiwifox/deathlogger/Main.java
@@ -2,45 +2,22 @@ package com.rainbowkiwifox.deathlogger;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.command.ConsoleCommandSender;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
     ConsoleCommandSender console = Bukkit.getConsoleSender();
+    public static Main instance = null;
     
     @Override
     public void onEnable() {
         console.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6[DeathLogger] &aПлагин запущен!"));
-        Bukkit.getPluginManager().registerEvents(this, this);
+        instance = this;
+        new EventListener().register();
     }
     @Override
     public void onDisable() {
         console.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6[DeathLogger] &aПлагин выключен."));
-    }
-    @EventHandler
-    public void handlerDeaths(PlayerDeathEvent e) {
-        Player victim = e.getEntity();
-        Player agressor = victim.getKiller();
-        Location victimLocation = victim.getLocation();
-        
-        int vlX = victimLocation.getBlockX();
-        int vlY = victimLocation.getBlockY(); 
-        int vlZ = victimLocation.getBlockZ();
-        String vlW = victimLocation.getWorld().getName();
-        
-        String vLastLocation = "(" + vlW + " " + vlX + "," + vlY + "," + vlZ + ")";
-        
-        Notifer n = new Notifer();
-        try {
-        n.notify(victim, agressor.getDisplayName(), vLastLocation);
-        } catch (Exception ex) {
-        String none = "&7не руками человека";
-        n.notify(victim, none, vLastLocation);
-        }
     }
 }
